26670e15d3edb506887143c2f10bd493b7e10dfd
Rajiv Kumar <rajivkumar@rajivs-mbp.resource.campus.njit.edu>
2022-02-21 12:39:04 -0500

    BasicNav

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -1,5 +1,5 @@
 <?php
-require(__DIR__ . "/../../lib/functions.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
@@ -29,7 +29,13 @@ require(__DIR__ . "/../../lib/functions.php");
 if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
-    $confirm = se($_POST, "confirm", "", false);
+    $confirm = se(
+        $_POST,
+        "confirm",
+        "",
+        false
+    );
+    //TODO 3
     $hasError = false;
     if (empty($email)) {
         echo "Email must not be empty";
@@ -37,38 +43,41 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
     }
     //sanitize
     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //validate
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email";
+        echo "Invalid email address";
         $hasError = true;
     }
     if (empty($password)) {
-        echo "Password must not be empty";
+        echo "password must not be empty";
         $hasError = true;
     }
     if (empty($confirm)) {
-        echo "Confirm Password must not be empty";
+        echo "Confirm password must not be empty";
         $hasError = true;
     }
     if (strlen($password) < 8) {
-        echo "Password must be >8 characters";
+        echo "Password too short";
         $hasError = true;
     }
-    if (strlen($password) > 0 && $password !== $confirm) {
+    if (
+        strlen($password) > 0 && $password !== $confirm
+    ) {
         echo "Passwords must match";
         $hasError = true;
     }
     if (!$hasError) {
-        //TODO 4
         echo "Welcome, $email";
+        //TODO 4
         $hash = password_hash($password, PASSWORD_BCRYPT);
         $db = getDB();
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+        $stmt = $db->prepare("INSERT INTO Users (email, password, username) VALUES(:email, :password, :username)");
         try {
-            $stmt->execute([":email" => $email, ":password" => $hash]);
+            $stmt->execute([":email" => $email, ":password" => $hash, ":username" => $username]);
             echo "Successfully registered!";
         } catch (Exception $e) {
             echo "There was a problem registering";
-            echo "<pre>" . var_export($e, true) . "</pre>";
+            "<pre>" . var_export($e, true) . "</pre>";
         }
     }
 }
